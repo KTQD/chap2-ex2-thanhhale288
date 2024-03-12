00bdd146af0dff2b78b2f36d91a387fedbed7417
Xuan Lam Pham <phamxuanlam@gmail.com>
2024-03-12 09:45:21 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 int main() {
     int height, width, perimeter;
 
-    // Prompting user input for height and width of the rectangle
     printf("Enter the height and width of the rectangle: ");
     scanf("%d %d", &height, &width);
 
-    // Calculating the perimeter of the rectangle
+  
     perimeter = 2 * (height + width);
 
-    // Printing the perimeter
+ 
     printf("Perimeter of the rectangle: %d\n", perimeter);
 
     return 0;
