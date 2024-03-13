ccf49d37366cbcb4571976f872680842add9605f
thanhhale288 <163239521+thanhhale288@users.noreply.github.com>
2024-03-13 15:11:00 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 int main() {
     float dai, rong, chuvi;
-
     printf("Nhap chieu dai cua hcn nheee: ");
     scanf("%f", &dai);
-
     printf("Nhap chieu rong cua hcn nheee: ");
     scanf("%f", &rong);
-
     chuvi = 2 * (dai + rong);
-
     printf("Chu vi cua hinh chu nhat neee = %.2f\n", chuvi);
-
     return 0;
 }
